e9e4c76b1de270155247e4f84617d6bedc53eb21
dtekluva <inyangete@gmail.com>
2025-01-16 08:32:09 +0100

    maintaining backend state

--- tasks.py
+++ tasks.py
@@ -89,62 +89,68 @@ def compile_for_wyre(data):
     print("DATA : ", data)
     
     # data = {"type":"data","time":"20211124101800","gwSN":"12109132830001","meterSN":"12109132830004","meterName":"adl400","meterStatus":"normal","ch":0,"ua":221,"ub":220.9,"uc":220.9,"ia":random.randint(1,50),"ib":random.randint(1,50),"ic":random.randint(1,50),"pa":random.randint(1,50),"pb":random.randint(1,50),"pc":random.randint(1,50),"p":random.randint(1,50),"qa":random.randint(1,50),"qb":random.randint(1,50),"qc":random.randint(1,50),"q":random.randint(1,50),"pf":random.randint(1,50),"pt":1,"ct":40,"wh":8.98,"wl":0.6,"varh":5.21,"varl":5.43}
-
+    
+    device_id = data["meterSN"] if not (data.get("ch", "")) else data["meterSN"] + "-" + str(data.get("ch", "")) # Add meter channel to nmeter number if the meter has a channel.
 
     try:
+        try:
+            pf = data["pf"] or 1
+        except:
+            pf = 0
+
         payload = {
-                    "device_id": data["meterSN"],
-                    "post_datetime": str(datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')),
-                    "post_date": str(datetime.datetime.now().strftime('%Y-%m-%d')),
-                    "post_time": str(datetime.datetime.now().strftime('%H:%M:%S')),
-                    "voltage_l1_l12":data["ua"],
-                    "voltage_l2_l23":data["ub"],
-                    "voltage_l3_l31":data["uc"],
-                    "current_l1":data["ia"]*40,
-                    "current_l2":data["ib"]*40,
-                    "current_l3":data["ic"]*40,
-                    "kw_l1":data["pa"]*40,
-                    "kw_l2":data["pb"]*40,
-                    "kw_l3":data["pc"]*40,
-                    "kvar_l1":data["qa"]*40,
-                    "kvar_l2":data["qb"]*40,
-                    "kvar_l3":data["qc"]*40,
-                    "kva_l1":data["pa"]*data["pf"],
-                    "kva_l2":data["pb"]*data["pf"],
-                    "kva_l3":data["pc"]*data["pf"],
-                    "power_factor_l1":0,
-                    "power_factor_l2":0,
-                    "power_factor_l3":0,
-                    "total_kw":data["p"]*40,
-                    "total_kvar":data["q"]*40,
-                    "total_kva":data["p"]*40*data["pf"],
-                    "total_pf":data["pf"],
-                    "avg_frequency":0,
-                    "neutral_current":0,
-                    "volt_thd_l1_l12":0,
-                    "volt_thd_l2_l23":0,
-                    "volt_thd_l3_l31":0,
-                    "current_thd_l1":0,
-                    "current_thd_l2":0,
-                    "current_thd_l3":0,
-                    "current_tdd_l1":0,
-                    "current_tdd_l2":0,
-                    "current_tdd_l3":0,
-                    "kwh_import":data["wh"]*data["ct"],
-                    "kwh_export":0,
-                    "kvarh_import":data["varh"]/1000,
-                    "kvah_total":0,
-                    "max_amp_demand_l1":0,
-                    "max_amp_demand_l2":0,
-                    "max_amp_demand_l3":0,
-                    "max_sliding_window_kw_demand":0,
-                    "accum_kw_demand":0,
-                    "max_sliding_window_kva_demand":0,
-                    "present_sliding_window_kw_demand":0,
-                    "present_sliding_window_kva_demand":0,
-                    "accum_kva_demand":0,
-                    "pf_import_at_maximum_kva_sliding_window_demand":0
-                }
+            "device_id": device_id,
+            "post_datetime": str(datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')),
+            "post_date": str(datetime.datetime.now().strftime('%Y-%m-%d')),
+            "post_time": str(datetime.datetime.now().strftime('%H:%M:%S')),
+            "voltage_l1_l12":data["ua"],
+            "voltage_l2_l23":data["ub"],
+            "voltage_l3_l31":data["uc"],
+            "current_l1":data["ia"]*data.get("ct", 1),
+            "current_l2":data["ib"]*data.get("ct", 1),
+            "current_l3":data["ic"]*data.get("ct", 1),
+            "kw_l1":data["pa"]*data.get("ct", 1) if data.get("ct", False) or not data.get("meterName", False) else data["pa"]/1000,
+            "kw_l2":data["pb"]*data.get("ct", 1) if data.get("ct", False) or not data.get("meterName", False) else data["pb"]/1000,
+            "kw_l3":data["pc"]*data.get("ct", 1) if data.get("ct", False) or not data.get("meterName", False) else data["pc"]/1000,
+            "kvar_l1":data["qa"]*data.get("ct", 1) if data.get("ct", False) or not data.get("meterName", False) else data["qa"]/1000,
+            "kvar_l2":data["qb"]*data.get("ct", 1) if data.get("ct", False) or not data.get("meterName", False) else data["qb"]/1000,
+            "kvar_l3":data["qc"]*data.get("ct", 1) if data.get("ct", False) or not data.get("meterName", False) else data["qc"]/1000,
+            "kva_l1":data["pa"]/pf if data.get("ct", False) or not data.get("meterName", False) else (data["pa"]/1000)/pf or 1,
+            "kva_l2":data["pb"]/pf if data.get("ct", False) or not data.get("meterName", False) else (data["pb"]/1000)/pf,
+            "kva_l3":data["pc"]/pf if data.get("ct", False) or not data.get("meterName", False) else (data["pa"]/1000)/pf,
+            "power_factor_l1":0,
+            "power_factor_l2":0,
+            "power_factor_l3":0,
+            "total_kw":data["p"]*data.get("ct", 1) if data.get("ct", False) or not data.get("meterName", False) else data["p"]/1000,
+            "total_kvar":data["q"]*data.get("ct", 1) if data.get("ct", False) or not data.get("meterName", False) else data["q"]/1000,
+            "total_kva":data["p"]*data.get("ct", 1)/pf if data.get("ct", False) or not data.get("meterName", False) else (data["p"]/1000)/pf,
+            "total_pf":data["pf"],
+            "avg_frequency":0,
+            "neutral_current":0,
+            "volt_thd_l1_l12":0,
+            "volt_thd_l2_l23":0,
+            "volt_thd_l3_l31":0,
+            "current_thd_l1":0,
+            "current_thd_l2":0,
+            "current_thd_l3":0,
+            "current_tdd_l1":0,
+            "current_tdd_l2":0,
+            "current_tdd_l3":0,
+            "kwh_import":data["wh"]*data.get("ct", 1),
+            "kwh_export":0,
+            "kvarh_import":data["varh"],
+            "kvah_total":0,
+            "max_amp_demand_l1":0,
+            "max_amp_demand_l2":0,
+            "max_amp_demand_l3":0,
+            "max_sliding_window_kw_demand":0,
+            "accum_kw_demand":0,
+            "max_sliding_window_kva_demand":0,
+            "present_sliding_window_kw_demand":0,
+            "present_sliding_window_kva_demand":0,
+            "accum_kva_demand":0,
+            "pf_import_at_maximum_kva_sliding_window_demand":0
+        }
                 
         datalogs_payload = {
                               "total_kw": payload["total_kw"],
@@ -163,14 +169,18 @@ def compile_for_wyre(data):
                           
         print("RAW LOGS", datalogs_payload)
         db_manager.write_to_readings_table(**payload)
+        print("DEVICE ID ####################", device_id)
         # print(payload)
-        if datalogs_payload["summary_energy_register_1"] == 0:
-            return dict()
+        if True: # datalogs_payload["summary_energy_register_1"] == 0:
+            print("FAILED CONDITION ####################", device_id)
+            print("SUMMARY ENERGY :::::::::", datalogs_payload["summary_energy_register_1"] )
+            #return dict()
             
-        response = requests.post("https://wyreng.xyz/posts/reading/", json=payload)
+        response = requests.post("https://backend.wyreng.com/posts/reading/", json=payload)
+        print("DEVICE ID ####################", device_id)
         print("RESPONSE : ", response.content)
-        response = requests.post("https://wyreng.xyz/posts/datalog/", json=datalogs_payload)
+        response = requests.post("https://backend.wyreng.com/posts/datalog/", json=datalogs_payload)
         print("DATALOG RESPONSE : ", response.content)
         
-    except KeyError:
+    except:
         print("error")
